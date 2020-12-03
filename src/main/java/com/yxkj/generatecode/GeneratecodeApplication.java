e3053da7eb2b47e3fb1aedae30a6bcd3c1009f15
SixSixDog <1159149401@qq.com>
2020-12-03 11:27:25 +0800

    Initial commit

---
+++ src/main/java/com/yxkj/generatecode/GeneratecodeApplication.java
@@ -0,0 +1,13 @@
+package com.yxkj.generatecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratecodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratecodeApplication.class, args);
+    }
+
+}
